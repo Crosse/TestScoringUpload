d542d799ec577d758e479b43c3ab63901ad906db
Seth Wright <seth@crosse.org>
2013-07-02 19:21:02 -0400

    Interface that connection helpers should adhere to

---
+++ TestScoringUpload/ITransferHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JMU.TestScoring
+{
+    public interface ITransferHelper
+    {
+        bool Connect(string server, string username, SecureString password);
+        void Disconnect();
+
+        bool UploadFile(string localPath, string remotePath);
+        bool WriteTextToFile(string remoteFilePath, string text);
+        bool CreateDirectory(string path);
+        string[] ListDirectory(string path);
+    }
+}

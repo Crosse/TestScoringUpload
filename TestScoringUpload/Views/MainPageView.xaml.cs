fec794490f6e54832d47fa5b63d7e014156a1d6f
Seth Wright <seth@crosse.org>
2013-07-03 15:53:36 -0400

    Work around an issue where the output text box will grow the window unconditionally.

--- TestScoringUpload/Views/MainPageView.xaml.cs
+++ TestScoringUpload/Views/MainPageView.xaml.cs
@@ -23,6 +23,8 @@ namespace JMU.TestScoring
         public MainPageView()
         {
             InitializeComponent();
+            this.Height = 350;
+            this.Width = 550;
         }
 
         private void Messages_TextChanged(object sender, TextChangedEventArgs e)
